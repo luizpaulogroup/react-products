b3ed04b4e3383737137fd68e23016a3b8991c0b5
Luiz <luizpaulogroup@gmail.com>
2019-05-29 13:28:53 -0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hellow Luiz</h1>
-    </div>
-  );
-}
+import Routes from './routes';
+
+import './styles.css';
+
+import Header from './components/Header';
+import Main from './pages/main';
+
+const App = () => (
+  <div className="App">
+      <Header />
+      <Routes />
+  </div>
+);
 
 export default App;
